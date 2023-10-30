8c8511be2d560b42e84a45d3d27e56c4167a3106
Jatin Lodhi <jatinlodhi111@gmail.com>
2023-10-30 18:16:04 +0530

    chore: sidebar fixed

--- src/assets/HomeIcon.tsx
+++ src/assets/HomeIcon.tsx
@@ -6,7 +6,7 @@ const HomeIcon = () => {
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="currentColor"
-    className="w-6 h-6 mr-3"
+    className="w-6 h-6 "
   >
     <path
       strokeLinecap="round"
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,29 +1,55 @@
-import { Link } from "react-router-dom";
 import { useAppSelector } from "../utils/hooks";
-import HomeIcon from "../assets/HomeIcon";
+import { AiFillHome } from "react-icons/ai";
+
+import { ImVideoCamera as ShortsIcon } from "react-icons/im";
+import { MdOndemandVideo as SubscriptionIcon } from "react-icons/md";
+import { SiYoutubemusic as YoutubeMusicIcon } from "react-icons/si";
+import { Link } from "react-router-dom";
 
 export const SideBar = () => {
   const isVisible = useAppSelector((store) => store.app.isSideBarOpen);
 
   if (isVisible == false) return;
+
   return (
     <div className="flex flex-col m-2 p-2 ">
       <div className="border m-2 p-2 ">
         <ul>
           <li className="p-2 hover:bg-gray-200">
             <Link to="/">
-              <div className="flex items-center ">
+              <div className="flex gap-2 items-center ">
                 <div>
-                  <HomeIcon />
+                  <AiFillHome />
                 </div>
                 <div>Home</div>
               </div>
             </Link>
           </li>
-          <li className="p-2 hover:bg-gray-200">Shorts</li>
-          <li className="p-2 hover:bg-gray-200">Subscriptions</li>
-          <li className="p-2 hover:bg-gray-200">Originals</li>
-          <li className="p-2 hover:bg-gray-200">Youtube Music</li>
+          <li className="p-2 hover:bg-gray-200">
+            <div className="flex gap-2 items-center ">
+              <div>
+                <ShortsIcon />
+              </div>
+              <div>Shorts</div>
+            </div>
+          </li>
+          <li className="p-2 hover:bg-gray-200">
+            <div className="flex gap-2 items-center ">
+              <div>
+                <SubscriptionIcon />
+              </div>
+              <div>Subscriptions</div>
+            </div>
+          </li>
+
+          <li className="p-2 hover:bg-gray-200">
+            <div className="flex gap-2 items-center ">
+              <div>
+                <YoutubeMusicIcon />
+              </div>
+              <div className="shrink-0">Youtube Music</div>
+            </div>
+          </li>
         </ul>
       </div>
       <div className="border m-2 p-2">
